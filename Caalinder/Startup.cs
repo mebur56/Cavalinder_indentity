72d396b6f8e376fc1ceab46bf88e297561ee9f00
Rubem Almeida <rubemdealmeida@hotmail.com>
2019-04-11 13:52:33 -0300

    Adicionar arquivos de projeto.

---
+++ Caalinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Caalinder.Startup))]
+namespace Caalinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
