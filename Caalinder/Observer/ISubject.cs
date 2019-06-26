d67dc00c6da145a50f6570f8849eaf68dc249207
Thomas Wicks <thomas.wicks@hotmail.com>
2019-06-26 16:31:33 -0300

    Observer

---
+++ Caalinder/Observer/ISubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caalinder.Observer
+{
+    public interface ISubject
+    {
+        void Registrar(IObserver observer);
+        void Remover(IObserver observer);
+        void EnviarEmail();
+    }
+
+    public interface IObserver
+    {
+        void ReceberEmail();
+    }
+}
---
+++ Caalinder/Observer/ObserverControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Caalinder.Observer
+{
+    public class Email
+    {
+        public string email { get; set; }
+    }
+    public class ObserverControl : IObservable<Email>
+    {
+        public List<IObserver<Email>> _user;
+        public Email _email;
+
+        public ObserverControl(Email email)
+        {
+            _user = new List<IObserver<Email>>();
+            _email = email;
+        }
+
+        public IDisposable Subscribe(IObserver<Email> user)
+        {
+            if (!_user.Contains(user))
+            {
+                _user.Add(user);
+            }
+            return new Disposer(_user, user);
+        }
+    }
+}
